1ec3b840a9773d98fa8de6a69c29e478bb163ef6
Carlos Colmenares <carlosshmx@hotmail.com>
2023-03-31 12:05:12 -0300

    Actualizadas imagenes de slideshow

--- src/components/Main/SlideShow.jsx
+++ src/components/Main/SlideShow.jsx
@@ -1,16 +1,16 @@
 function SlideShow(){
     return(
-        <>
+        <div className="d-none d-md-block">
             <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner main-slideshow">
     <div className="carousel-item active " data-bs-interval="5000">
-      <img src="https://res.cloudinary.com/carlosshmc/image/upload/v1626985087/Regalarte/df095418-07fd-442c-8a04-008404604d73-01_esntcf.jpg" className="d-block w-100 " alt="..."/>
+      <img src="https://res.cloudinary.com/carlosshmc/image/upload/v1680273095/Banners%20Regalarte/Disen%CC%83o_sin_ti%CC%81tulo_whxn1s.png" className="d-block w-100 " alt="..."/>
     </div>
     <div className="carousel-item" data-bs-interval="5000">
-      <img src="https://res.cloudinary.com/carlosshmc/image/upload/v1677018362/regalarte_letras_nml55d.png" className="d-block w-100 " alt="..."/>
+      <img src="https://res.cloudinary.com/carlosshmc/image/upload/v1680273902/Banners%20Regalarte/Sumer_ikx84b.png" className="d-block w-100 " alt="..."/>
     </div>
     <div className="carousel-item" data-bs-interval="5000">
-      <img src="https://res.cloudinary.com/carlosshmc/image/upload/v1626984736/Regalarte/100035400698-189450-01_szzok6.jpg" className="d-block w-100 " alt="..."/>
+      <img src="https://res.cloudinary.com/carlosshmc/image/upload/v1680274523/Banners%20Regalarte/Deleivery_free_vcjivw.png" className="d-block w-100 " alt="..."/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -25,7 +25,7 @@ function SlideShow(){
 
         
         
-        </>
+        </div>
         
 
     )
